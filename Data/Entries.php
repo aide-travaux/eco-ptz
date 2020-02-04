8e4c276a47497fa131c78bde8e475e0175e1f24e
Adrien Rosi <adrienrosi@gmail.com>
2020-02-04 17:58:37 +0100

    Add packages

--- Data/Entries.php
+++ Data/Entries.php
@@ -2,8 +2,16 @@
 
 namespace AideTravaux\EcoPTZ\Data;
 
-abstract class Entries
+use AideTravaux\Core\Entries as BaseEntries;
+use AideTravaux\Anah\Categorie\Entries as AnahCategorieEntries;
+
+abstract class Entries extends BaseEntries
 {
+    /**
+     * @property array
+     */
+    const CATEGORIES_ANAH = AnahCategorieEntries::CATEGORIES_ANAH;
+
     /**
      * @property array
      */
@@ -13,33 +21,6 @@ abstract class Entries
         'type_projet_eptz_3' => 'Bouquet de 3 travaux ou plus',
         'type_projet_eptz_4' => 'Performance énergétique globale',
         'type_projet_eptz_5' => 'Assainissement non collectif'
-    ];
-
-    /**
-     * @property array
-     */
-    const TYPES_LOGEMENT = [
-        'type_logement_1' => 'Maison individuelle',
-        'type_logement_2' => 'Appartement',
-        'type_logement_3' => 'Bâtiment collectif'
-    ];
-
-    /**
-     * @property array
-     */
-    const STATUTS_OCCUPANT_LOGEMENT = [
-        'statut_occupant_logement_1' => 'Propriétaire occupant',
-        'statut_occupant_logement_2' => 'Propriétaire bailleur',
-        'statut_occupant_logement_3' => 'Locataire',
-        'statut_occupant_logement_4' => 'Occupant à titre gratuit'
-    ];
-
-    /**
-     * @property array
-     */
-    const TYPES_OCCUPATION_LOGEMENT = [
-        'type_occupation_logement_1' => 'Résidence principale',
-        'type_occupation_logement_2' => 'Résidence secondaire'
     ];
 
 }
